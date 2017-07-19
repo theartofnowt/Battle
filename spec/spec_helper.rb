d9d80a9cb18ee52908780bc969fa78d7647f999b
theartofnowt <michaelgordon.brown1@gmail.com>
2017-07-19 16:01:35 +0100

    dummy version

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require File.join(File.dirname(__FILE__), '..', 'app.rb')
 
 require File.join(File.dirname(__FILE__), 'features/web_helpers.rb')
 
+
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
